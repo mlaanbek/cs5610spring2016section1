049c70ddc0089e2e31d1435dbf46715d05c91fd7
Marek Laanbek <marek.laanbek@gmail.com>
2016-07-27 09:15:47 +0300

    Add login view placeholder

--- public/project/omdb/client/config.js
+++ public/project/omdb/client/config.js
@@ -11,6 +11,9 @@
             .when("/search", {
                 templateUrl: "views/search/search.view.html"
             })
+            .when("/login", {
+                templateUrl: "views/login/login.view.html"
+            })
             .otherwise({
                 redirectTo: "/homr"
             });
