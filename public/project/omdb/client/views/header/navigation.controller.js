5b65c8b0219668e9038570eee5ad6a53b66a856e
Marek Laanbek <marek.laanbek@gmail.com>
2016-07-27 09:12:23 +0300

    Use $location service to highlight tabs

--- public/project/omdb/client/views/header/navigation.controller.js
+++ public/project/omdb/client/views/header/navigation.controller.js
@@ -3,7 +3,7 @@
         .module("OmdbApp")
         .controller("NavigationController", NavigationController);
 
-    function NavigationController() {
+    function NavigationController($location) {
 
         // Reference to the current instance of the controller. We'll use it instead of using $scope.
         // The aim is to make it more clear what to what controller a variable binds to.
@@ -13,7 +13,7 @@
         // It's a good practice to encapsulate anything that's has to execute when the controller
         // is first initialized. Just to make it explicit.
         function init() {
-
+            vm.$location = $location;
         }
         init();
     }
