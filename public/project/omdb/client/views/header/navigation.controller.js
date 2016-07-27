2a5b0d664cc43d44db3e71d38f0e19bcf165d1e5
Marek Laanbek <marek.laanbek@gmail.com>
2016-07-27 09:08:12 +0300

    Add navigation controller

---
+++ public/project/omdb/client/views/header/navigation.controller.js
@@ -0,0 +1,20 @@
+(function () {
+    angular
+        .module("OmdbApp")
+        .controller("NavigationController", NavigationController);
+
+    function NavigationController() {
+
+        // Reference to the current instance of the controller. We'll use it instead of using $scope.
+        // The aim is to make it more clear what to what controller a variable binds to.
+        // vm - view model
+        var vm = this;
+
+        // It's a good practice to encapsulate anything that's has to execute when the controller
+        // is first initialized. Just to make it explicit.
+        function init() {
+
+        }
+        init();
+    }
+})();
