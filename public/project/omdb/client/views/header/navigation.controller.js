e401d7f051b6978d4b85fd281b8d56e1c5ff618b
Marek Laanbek <marek.laanbek@gmail.com>
2016-07-28 17:39:06 +0300

    add logout link

--- public/project/omdb/client/views/header/navigation.controller.js
+++ public/project/omdb/client/views/header/navigation.controller.js
@@ -3,12 +3,14 @@
         .module("OmdbApp")
         .controller("NavigationController", NavigationController);
 
-    function NavigationController($location) {
+    function NavigationController($location, UserService) {
 
         // Reference to the current instance of the controller. We'll use it instead of using $scope.
         // The aim is to make it more clear what to what controller a variable binds to.
         // vm - view model
         var vm = this;
+
+        vm.logout = logout
 
         // It's a good practice to encapsulate anything that's has to execute when the controller
         // is first initialized. Just to make it explicit.
@@ -16,5 +18,10 @@
             vm.$location = $location;
         }
         init();
+
+        function logout() {
+            UserService.setCurrentUser(null);
+            $location.url("/home");
+        }
     }
 })();
