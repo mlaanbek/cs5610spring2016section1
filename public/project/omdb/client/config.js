cfc257b87b178684bd0b58567721f632a931d811
Marek Laanbek <marek.laanbek@gmail.com>
2016-07-30 17:03:54 +0300

    added search controller

--- public/project/omdb/client/config.js
+++ public/project/omdb/client/config.js
@@ -14,6 +14,8 @@
             })
             .when("/search", {
                 templateUrl: "views/search/search.view.html",
+                controller: "SearchController",
+                controllerAs: "model",
                 resolve: {
                     getLoggedIn: getLoggedIn
                 }
