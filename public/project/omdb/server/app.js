c00c9cc23dd439f32570f97f4fd4bfd9aa721207
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-02 09:08:46 +0300

    added movie service model

--- public/project/omdb/server/app.js
+++ public/project/omdb/server/app.js
@@ -6,5 +6,7 @@ module.exports = function(app) {
     // load the model and pass it to the service
     var userModel = require("./models/user.model.server.js")();
     var userService = require("./services/user.service.server.js")(app, userModel);
-    var movieService = require("./services/movie.service.server.js")(app);
+    
+    var movieModel = require("./models/movie.model.server.js")();
+    var movieService = require("./services/movie.service.server.js")(app, movieModel);
 }
--- public/project/omdb/server/services/movie.service.server.js
+++ public/project/omdb/server/services/movie.service.server.js
@@ -1,10 +1,12 @@
-module.exports = function (app) {
+module.exports = function (app, movieModel) {
     app.post("/api/project/user/:userId/movie/:imdbID", userLikesMovie);
 
     function userLikesMovie(req, res) {
         var userId = req.params.userId;
         var imdbId = req.params.imdbID;
-        console.log([userId, imdbID]);
+
+        var movie = movieModel.findMovieByImdbID(imdbId);
+        console.log([userId, imdbId, movie]);
         res.send(200);
     }
 }
