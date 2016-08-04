99070be739a1586f7f46184117b153e7909ee320
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-04 09:49:26 +0300

    added MongoDB to Database - START

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ var app = express();
 var bodyParser = require('body-parser');            // to parse json from the body
 var cookieParser = require('cookie-parser');        // can parse cookies from the header
 var session = require('express-session');           // node.js module that provides high-level api to keep objects per session that initiated by cookies
+// install and require the mongoose library
+var mongoose = require('mongoose');
+
+// create a default connection string
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610projectomdb';
+
+/*
+*   If remote server is used, use this connection
+**/
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
+            process.env.OPENSHIFT_APP_NAME;
+}
+
+// connect to the database
+var db = mongoose.connect(connectionString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
