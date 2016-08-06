dfc299bafd91c123dd471a0b41d1d54b20f45014
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-06 20:02:49 +0300

    Refactor findUserLikes() Service

--- public/project/omdb/server/services/movie.service.server.js
+++ public/project/omdb/server/services/movie.service.server.js
@@ -1,6 +1,32 @@
 module.exports = function (app, movieModel, userModel) {
     app.post("/api/project/user/:userId/movie/:imdbID", userLikesMovie);
 
+    function findUserLikes(req, res) {
+        var imdbID = req.params.imdbID;
+
+        var movie = null;
+        movieModel
+            .findMovieByImdbID(imdbID)
+            .then(
+                function (doc) {
+                    movie = doc;
+                    return userModel.findUsersByIds(movie.likes);
+                },
+                function (err) {
+                    res.status(400).send(err);
+                }
+            )
+            .then(
+                function (users) {
+                    movie.userLikes = users;
+                    res.json(movie);
+                },
+                function (err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
     function userLikesMovie(req, res) {
         var movieOmdb = req.body;
         var userId = req.params.userId;
