aacc33485cb53fedf472f0d45d64944978186ca1
Marek Laanbek <marek.laanbek@gmail.com>
2016-08-09 09:12:36 +0300

    Highlight the like button

--- public/project/omdb/client/views/details/details.controller.js
+++ public/project/omdb/client/views/details/details.controller.js
@@ -25,6 +25,15 @@
 
         function favorite(movie) {
             if (currentUser) {
+                // the client does not know that a model has changed (user has liked the movie and
+                // it's been recorded in the database but the client is unaware of it.
+                // To change the like star highlight we cach the changed state.
+                // Next time the will have the from the database anyway.
+
+                // the better approach however would be to add this into the promise. That way we assure
+                // that movie will be liked only if the promise comes back successfully
+                vm.movie.likes = [];
+                vm.movie.likes.push(currentUser._id);
                 MovieService
                     .setUserLikesMovie(currentUser._id, movie);
             } else {
